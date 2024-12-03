c80db62a75c4a7585f9a1bcee134a1c9cdd481f9
SYAN-01 <moneoy561@naver.com>
2024-12-03 14:40:55 +0900

    데이터 옮기기

---
+++ src/comp/inp/input01.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export default function InputStudy() {
+
+    const [inp, setInp] = useState('');
+
+    function send() {
+        alert(inp);
+
+        localStorage.setItem('study')
+    }
+
+    return (
+        <div>
+            <h1>InputStudy</h1>
+            <input
+                type='text'
+                value={inp}
+                onChange={ e=> setInp(e.target.value) } />
+
+                <input type='button' value='전송' onClick={send}/>
+        </div>
+    )
+}
